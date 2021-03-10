b45f64bc69137aee0c91d1c1992608b1fa65cf97
cnjm <1837847333@qq.com>
2021-03-10 14:37:15 +0800

    f

--- leetcode/1.两数之和.js
+++ leetcode/1.两数之和.js
@@ -11,13 +11,21 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  for(let i = 0;i<nums.length-1;i++){
-    for(let j = i+1;j<nums.length;j++){
-      if(nums[i]+nums[j]==target){
-        return [i,j];
-      }
+  const map = new Map();
+  for(let i = 0;i<nums.length;i++){
+    if(map.has(target - nums[i])){
+      return [map.get(target - nums[i]),i];
+    }else{
+      map.set(nums[i],i)
     }
   }
+  // for(let i = 0;i<nums.length-1;i++){
+  //   for(let j = i+1;j<nums.length;j++){
+  //     if(nums[i]+nums[j]==target){
+  //       return [i,j];
+  //     }
+  //   }
+  // }
 };
 // @lc code=end
 
